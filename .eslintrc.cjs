56170da4ad35c3d50bf80a24206da52b4ab192b6
Matthew Vance <mavance44@gmail.com>
2024-01-22 16:37:47 -0500

    init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: ["./**/*.{js,cjs}"],
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# United States National Register of Historic Places
