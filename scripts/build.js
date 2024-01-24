6bcafe0e04d1a02d014ebecb33fa39639283e645
Matthew Vance <mavance44@gmail.com>
2024-01-24 11:47:56 -0500

    configure esbuild

---
+++ scripts/build.js
@@ -0,0 +1,10 @@
+import * as esbuild from "esbuild";
+
+await esbuild.build({
+  bundle: true,
+  entryPoints: ["src/main.ts"],
+  format: "esm",
+  outfile: "dist/index.js",
+  packages: "external",
+  platform: "node",
+});
