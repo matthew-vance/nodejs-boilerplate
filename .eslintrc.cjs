07b2834d37c42dd4185069e3ad85293fa0b1279e
Matthew Vance <mavance44@gmail.com>
2024-01-24 09:27:01 -0500

    add import sorting to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   extends: [
     "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
     "prettier",
@@ -10,10 +12,23 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint"],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "simple-import-sort", "import"],
   root: true,
   env: {
     node: true,
+  },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
   },
   overrides: [
     {
